a242e424a8e7bffd5a0e475682600efb84f48650
kimjeff49 <jeffkkim612@gmail.com>
2024-11-20 13:30:57 -0500

    adding in stats

--- src/lib/dnd-class.ts
+++ src/lib/dnd-class.ts
@@ -19,7 +19,26 @@ export const DRUID = {
 }
 export const FIGHTER = {
   name: 'Fighter',
-  stats: {},
+  stats: {
+    strength: 15,
+    vigor: 15,
+    agility: 15,
+    dexterity: 15,
+    will: 15,
+    knowledge: 15,
+    resourcefulness: 15,
+    health: 100,
+    memory_capacity: 9,
+    utility_effectiveness: 0,
+    luck: 0,
+    health_recovery_bonus: 0,
+    spell_recovery_bonus: 65.5,
+    move_speed: 300,
+    action_speed: 0,
+    manual_dexterity: 0,
+    spell_casting_speed: 0,
+    
+  },
 }
 export const CLERIC = {
   name: 'Cleric',
